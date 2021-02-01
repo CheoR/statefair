3e9771e09c1564a787f8ce6d652fcea87c999f69
CheoR <cheo.roman+github@gmail.com>
2021-01-31 20:20:43 -0600

    Add event listner to ride column. Blue square appears per ride ticket bought.

--- scripts/rides/RideTicketHolders.js
+++ scripts/rides/RideTicketHolders.js
@@ -1,8 +1,13 @@
-const contentTarget = document.querySelector(".rides")
+let contentTarget = document.querySelector(".rides")
 const eventHub = document.querySelector("#state-fair")
 
 export const RideTicketHolders = () => {
-    eventHub.addEventListener("", customEvent => {
+    eventHub.addEventListener("rideTicketPurchased", customEvent => {
+        if(customEvent.detail.ticketType === "ride") {
+            contentTarget.innerHTML += `
+            <div class="person rider"></div>
+            `
+        }
     })
 }
 
