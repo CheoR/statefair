accb14f4221b691cfcd7e2be468b8103573dbfb0
CheoR <cheo.roman+github@gmail.com>
2021-02-13 18:11:48 -0600

    fix - refactor fullPackgeTicketHolder

--- scripts/fullPackage/FullPackageTicketHolder.js
+++ scripts/fullPackage/FullPackageTicketHolder.js
@@ -1,28 +1,12 @@
-import { TicketBooth } from "../TicketBooth.js"
-
 const eventHub = document.querySelector("#state-fair")
 
 export const FullPackageTicketHolder = () => {
  eventHub.addEventListener("fullPackageTicketPurchased", customEvent => {
   if(customEvent.detail.ticketType === "package") {
-   const ticketBoothBtns = document.querySelectorAll(".ticketBooth button")
-   
-   // So each attraction adds one more.
-   ticketBoothBtns.forEach(( btn ) => {
-    if(btn.id !== "fullPackageTicket") {
-     btn.click();
-    }
-   }) // end ticketBoothBtns
-
-   // Sigify last ticket was bought as a package ticket
-   const attractions = document.querySelectorAll(".attractions .attraction")
-   
-   attractions.forEach(( attraction ) => {
-    const lastPerson = attraction.querySelector(".person:last-child")
-
-    lastPerson.classList.add("bigSpender")
-   })
+   const attractions = document.getElementsByClassName("people")
+   for(const attraction of attractions) {
+    attraction.innerHTML += `<div class="person bigSpender"></div>`
+   } // attractions.forEach
   } // end if
- })
-
-}
+ }) // eventHub - fullPackageTicketPurchased
+} // FullPackageTicketHolder
